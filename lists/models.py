7a7ca9afa9e650587872c73da77cfc02fda8b108
Chaua Queirolo <chaua@users.noreply.github.com>
2022-05-24 22:17:23 -0300

    :sparkles: Inclusão do campo prioridade no modelo e correção do texto da tabela

--- lists/models.py
+++ lists/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class List(models.Model):
-	pass
+    pass
+
 
 class Item(models.Model):
-	text = models.TextField(default='')
-	list = models.ForeignKey(List,on_delete=models.SET_DEFAULT,default=None)
+    text = models.TextField(default='')
+    list = models.ForeignKey(List, on_delete=models.SET_DEFAULT, default=None)
+    prioridade = models.TextField(default='')
