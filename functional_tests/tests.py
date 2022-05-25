6d7e5a7c7d0b40418017f68e7be914fa36c8bbf0
Chaua Queirolo <chaua@users.noreply.github.com>
2022-05-24 21:55:49 -0300

    :sparkles: Atualização dos testes funcionais

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+
+from selenium.webdriver.support.select import Select
 
 MAX_WAIT = 10
 
@@ -29,101 +32,96 @@ class NewVsitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_for_one_user(self):
-        # Edith ouviu falar de uma nova aplicação online interessante
-        # para lista de tarefas. Ela decide verificar a homepage
-
+    def test_nova_situacao_problema(self):
+        # Edith ouviu falar que agora a aplicação online de lista de tarefas
+        # aceita definir prioridades nas tarefas do tipo baixa, média e alta
+        # Ela decide verificar a homepage
         self.browser.get(self.live_server_url)
 
         # Ela percebe que o título da página e o cabeçalho mencionam
-        # listas de tarefas (to-do)
+        # listas de tarefas com prioridade (priority to-do)
+        self.assertIn('priority to-do', self.browser.title)
+        header_text = self.browser.find_element(by=By.NAME, value='h1').text
+        self.assertIn('priority to-do', header_text)
 
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
+        # Ela é convidada a inserir um item de tarefa e a prioridade da
+        # mesma imediatamente
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
-        # Ela é convidada a inserir um item de tarefa imediatamente
+        selectbox = Select(self.browser.find_element(by=By.ID, value='prioridade'))
 
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        # Ela digita "Buy peacock feathers" (Comprar penas de pavão)
-        # em uma nova caixa de texto (o hobby de Edith é fazer iscas
-        # para pesca com fly)
-
-        inputbox.send_keys('Buy peacock feathers')
+        # Ela digita "Comprar anzol" em uma nova caixa de texto
+        # e assinala prioridade alta no campo de seleção de prioridades
+        inputbox.send_keys('Comprar anzol')
+        selectbox.select_by_visible_text('alta')
 
         # Quando ela tecla enter, a página é atualizada, e agora
-        # a página lista "1 - Buy peacock feathers" como um item em
-        # uma lista de tarefas
-
+        # a página lista "1 - Comprar anzol - prioridade alta"
+        # como um item em uma lista de tarefas
         inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1 - Comprar anzol - prioridade alta')
 
         # Ainda continua havendo uma caixa de texto convidando-a a
-        # acrescentar outro item. Ela insere "Use peacock feathers
-        # to make a fly" (Usar penas de pavão para fazer um fly -
-        # Edith é bem metódica)
+        # acrescentar outro item. Ela insere "Comprar cola instantâne"
+        # e assinala prioridade baixa pois ela ainda tem cola suficiente
+        # por algum tempo
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys("Comprar cola instantânea")
         inputbox.send_keys(Keys.ENTER)
+        selectbox.select_by_visible_text('baixa')
 
         # A página é atualizada novamente e agora mostra os dois
-        # itens em sua lista
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # itens em sua lista e as respectivas prioridades
+
+        self.wait_for_row_in_list_table('1 - Comprar anzol - prioridade alta')
+        self.wait_for_row_in_list_table('2 - Comprar cola instantânea - prioridade baixa')
 
     # Edith se pergunta se o site lembrará de sua lista. Então
     # ela nota que o site gerou um URL único para ela -- há um
     # pequeno texto explicativo para isso.
 
     # Ela acessa essa URL -- sua lista de tarefas continua lá.
-
-    # Satisfeita, ela volta a dormir
-
-    def test_multiple_users_can_start_lists_at_different_urls(self):
-        # Edith inicia uma nova lista de tarefas
-        self.browser.get(self.live_server_url)
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy peacock feathers')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        # Ela percebe que sua lista te um URL único
-        edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-
-        # Agora um novo usuário, Francis, chega ao site
-
-        ## Usamos uma nova versão do nagegador para garantir que nenhuma
-        ## informação de Edith está vindo de cookies, etc
-
-        self.browser.quit()
-        self.browser = webdriver.Firefox()
-
-        # Francis acessa a página inicial. Não há sinal da lista de Edith
-        self.browser.get(self.live_server_url)
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('make a fly', page_text)
-
-        # Francis inicia uma nova lista inserindo um novo item.
-        inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy milk')
-        inputbox.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_table('1: Buy milk')
-
-        # Francis obtém seu próprio URL exclusivo
-        francis_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')
-        self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # Novamente não há sinal algum da lista de Edith
-        page_text = self.browser.find_element_by_tag_name('body').text
-        self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertIn('Buy milk', page_text)
+    # def test_multiple_users_can_start_lists_at_different_urls(self):
+    #     # Edith inicia uma nova lista de tarefas
+    #     self.browser.get(self.live_server_url)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy peacock feathers')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #
+    #     # Ela percebe que sua lista te um URL único
+    #     edith_list_url = self.browser.current_url
+    #     self.assertRegex(edith_list_url, '/lists/.+')
+    #
+    #     # Agora um novo usuário, Francis, chega ao site
+    #
+    #     ## Usamos uma nova versão do nagegador para garantir que nenhuma
+    #     ## informação de Edith está vindo de cookies, etc
+    #
+    #     self.browser.quit()
+    #     self.browser = webdriver.Firefox()
+    #
+    #     # Francis acessa a página inicial. Não há sinal da lista de Edith
+    #     self.browser.get(self.live_server_url)
+    #     page_text = self.browser.find_element_by_tag_name('body').text
+    #     self.assertNotIn('Buy peacock feathers', page_text)
+    #     self.assertNotIn('make a fly', page_text)
+    #
+    #     # Francis inicia uma nova lista inserindo um novo item.
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Buy milk')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     self.wait_for_row_in_list_table('1: Buy milk')
+    #
+    #     # Francis obtém seu próprio URL exclusivo
+    #     francis_list_url = self.browser.current_url
+    #     self.assertRegex(edith_list_url, '/lists/.+')
+    #     self.assertNotEqual(francis_list_url, edith_list_url)
+    #
+    #     # Novamente não há sinal algum da lista de Edith
+    #     page_text = self.browser.find_element_by_tag_name('body').text
+    #     self.assertNotIn('Buy peacock feathers', page_text)
+    #     self.assertIn('Buy milk', page_text)
 
     # Fim
